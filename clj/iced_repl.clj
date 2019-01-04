f15207fc8fe0bfb4089e4740a3091f307213e415
liquidz <liquidz.uo@gmail.com>
2019-01-04 16:36:41 +0900

    Update iced command to toggle cljs feature with `--cljs` option #73

--- clj/iced_repl.clj
+++ clj/iced_repl.clj
@@ -1,8 +1,9 @@
 (ns iced-repl
   (:require [nrepl.cmdline :as cmd]))
 
-(def middlewares
-  ["cider.nrepl/wrap-complete"
+(def clj-middlewares
+  ["cider.nrepl/wrap-classpath"
+   "cider.nrepl/wrap-complete"
    "cider.nrepl/wrap-debug"
    "cider.nrepl/wrap-format"
    "cider.nrepl/wrap-info"
@@ -15,9 +16,13 @@
    "cider.nrepl/wrap-test"
    "cider.nrepl/wrap-trace"
    "cider.nrepl/wrap-undef"
-   "cider.piggieback/wrap-cljs-repl"
    "refactor-nrepl.middleware/wrap-refactor"
    "iced.nrepl/wrap-iced"])
 
-(defn -main []
-  (cmd/-main "-m" (pr-str middlewares)))
+(def cljs-middlewares
+  (conj clj-middlewares "cider.piggieback/wrap-cljs-repl"))
+
+(defn -main [& args]
+  (if (= "with-cljs-middleware" (some-> args seq first))
+    (cmd/-main "-m" (pr-str cljs-middlewares))
+    (cmd/-main "-m" (pr-str clj-middlewares))))
