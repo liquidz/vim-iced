0fe8734968bf5b11e30b6e1471d5bc5e77bdb2ed
liquidz <liquidz.uo@gmail.com>
2019-08-28 16:02:25 +0900

    Fix shadow_cljs.clj to use qualified symbols

--- clj/load_files/shadow_cljs.clj
+++ clj/load_files/shadow_cljs.clj
@@ -1,31 +1,34 @@
 (def ___vim-iced-shadow-cljs-ns___ 'vim-iced.loaded.shadow-cljs)
-(create-ns ___vim-iced-shadow-cljs-ns___)
+(clojure.core/create-ns ___vim-iced-shadow-cljs-ns___)
 
-(intern
+(clojure.core/intern
  ___vim-iced-shadow-cljs-ns___
  'validate-config
- (fn [vim-iced-home-dir config-path]
-   (require 'clojure.edn
+ (clojure.core/fn [vim-iced-home-dir config-path]
+   (clojure.core/require 'clojure.edn
             'clojure.java.io
             'clojure.set
             'clojure.string)
-   (let [read-edn #(-> % slurp clojure.edn/read-string)
-         shadow-cljs-config (read-edn config-path)
-         deps-edn (read-edn (clojure.java.io/file vim-iced-home-dir "deps.edn"))
-         target-deps #{'cider/cider-nrepl 'refactor-nrepl 'iced-nrepl}
-         deps-diff (clojure.set/difference
-                    (->> (:deps deps-edn)
-                         (filter #(target-deps (first %)))
-                         (map (juxt first (comp :mvn/version second)))
-                         set)
-                    (->> (:dependencies shadow-cljs-config)
-                         (filter #(target-deps (first %)))
-                         set))
-         mdws-diff (clojure.set/difference
-                    (set (map symbol (:__middlewares__ deps-edn)))
-                    (set (get-in shadow-cljs-config [:nrepl :middleware])))]
+   (clojure.core/let [read-edn #(clojure.core/-> % clojure.core/slurp clojure.edn/read-string)
+                      shadow-cljs-config (read-edn config-path)
+                      deps-edn (read-edn (clojure.java.io/file vim-iced-home-dir "deps.edn"))
+                      target-deps #{'cider/cider-nrepl 'refactor-nrepl 'iced-nrepl}
+                      deps-diff (clojure.set/difference
+                                  (clojure.core/->> (:deps deps-edn)
+                                                    (clojure.core/filter #(target-deps (clojure.core/first %)))
+                                                    (clojure.core/map (clojure.core/juxt clojure.core/first
+                                                                                         (clojure.core/comp
+                                                                                           :mvn/version
+                                                                                           clojure.core/second)))
+                                                    clojure.core/set)
+                                  (clojure.core/->> (:dependencies shadow-cljs-config)
+                                                    (clojure.core/filter #(target-deps (clojure.core/first %)))
+                                                    clojure.core/set))
+                      mdws-diff (clojure.set/difference
+                                  (clojure.core/set (clojure.core/map clojure.core/symbol (:__middlewares__ deps-edn)))
+                                  (clojure.core/set (clojure.core/get-in shadow-cljs-config [:nrepl :middleware])))]
      (clojure.string/join
       "\n"
-      (concat
-       (map (fn [[sym ver]] (str "Missing dependency: " sym " => " ver)) deps-diff)
-       (map (fn [mdw]       (str "Missing middleware: " mdw)) mdws-diff))))))
+      (clojure.core/concat
+       (clojure.core/map (clojure.core/fn [[sym ver]] (clojure.core/str "Missing dependency: " sym " => " ver)) deps-diff)
+       (clojure.core/map (clojure.core/fn [mdw]       (clojure.core/str "Missing middleware: " mdw)) mdws-diff))))))
