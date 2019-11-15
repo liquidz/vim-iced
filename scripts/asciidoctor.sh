b1f75d9f509d4f50631c5955280babbf9ac60306
liquidz <liquidz.uo@gmail.com>
2019-11-16 06:59:29 +0900

    Update asciidoctor.sh to copy assets

--- scripts/asciidoctor.sh
+++ scripts/asciidoctor.sh
@@ -2,6 +2,12 @@
 
 echo -n 'start ... '
 
+if [ -d target/html/assets ]; then
+    echo -n 'copying assets ... '
+    \rm -rf target/html/assets
+    \cp -pr  doc/pages/assets target/html/assets
+fi
+
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor \
     asciidoctor -o target/html/index.html doc/pages/index.adoc
 
