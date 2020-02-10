a6c819878a16985c2e90918217c6c39cb59b14ed
liquidz <liquidz.uo@gmail.com>
2020-02-11 07:06:01 +0900

    Fix fetching document code to work with cljs

--- clj/template/socket_repl_document.clj
+++ clj/template/socket_repl_document.clj
@@ -1,4 +1,4 @@
-(when-let [{:keys [name arglists doc]} (some-> '%s resolve meta)]
+(when-let [{:keys [name arglists doc]} (meta (resolve '%s))]
   (when (or arglists doc)
     (let [str-join (fn [sep ls] (apply str (drop-last (interleave ls (repeat sep)))))]
       (cond-> [(str "*" name "*")]
