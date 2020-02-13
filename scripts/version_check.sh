4357e4c9e658db3551da3aa5b4152d84733342b8
liquidz <liquidz.uo@gmail.com>
2020-02-13 14:54:39 +0900

    Update version checker to validate installer's version

--- scripts/version_check.sh
+++ scripts/version_check.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 VERSION=$(grep 'Version: ' ${SCRIPT_DIR}/../doc/vim-iced.txt | cut -d' ' -f2)
 
+### vim-iced verion
 VERSION_NUM=''
 for n in $(echo ${VERSION} | tr '.' '\n'); do
     VERSION_NUM="${VERSION_NUM}$(printf %02d ${n})"
@@ -18,5 +19,32 @@ fi
 grep "== ${VERSION}" ${SCRIPT_DIR}/../CHANGELOG.adoc
 if [ $? -ne 0 ]; then
     echo 'version_check: There is no corresponding section in CHANGELOG'
+    exit 1
+fi
+
+### borkdude/jet version
+JET_URL='https://github.com/borkdude/jet/releases.atom'
+JET_VERSION=$(curl -s ${JET_URL} | grep '<title>' | head -n 2 | tail -n 1 | sed 's/[^0-9.]//g')
+grep "version='${JET_VERSION}'" ${SCRIPT_DIR}/../installer/jet.sh
+if [ $? -ne 0 ]; then
+    echo 'version_check: Version num in installer/jet.sh is outdated (${JET_VERSION})'
+    exit 1
+fi
+
+### greglook/cljstyle version
+CLJSTYLE_URL='https://github.com/greglook/cljstyle/releases.atom'
+CLJSTYLE_VERSION=$(curl -s ${CLJSTYLE_URL} | grep '<title>' | head -n 2 | tail -n 1 | sed 's/[^0-9.]//g')
+grep "version='${CLJSTYLE_VERSION}'" ${SCRIPT_DIR}/../installer/cljstyle.sh
+if [ $? -ne 0 ]; then
+    echo 'version_check: Version num in installer/cljstyle.sh is outdated (${CLJSTYLE_VERSION})'
+    exit 1
+fi
+
+### kkinnear/zprint version
+ZPRINT_URL='https://github.com/kkinnear/zprint/releases.atom'
+ZPRINT_VERSION=$(curl -s ${ZPRINT_URL} | grep '<title>' | head -n 2 | tail -n 1 | sed 's/[^0-9.]//g')
+grep "version='${ZPRINT_VERSION}'" ${SCRIPT_DIR}/../installer/zprint.sh
+if [ $? -ne 0 ]; then
+    echo 'version_check: Version num in installer/zprint.sh is outdated (${ZPRINT_VERSION})'
     exit 1
 fi
