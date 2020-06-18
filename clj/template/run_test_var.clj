a9d750e7ddb06f796d9319ba26c43f1a1078bcad
liquidz <liquidz.uo@gmail.com>
2020-06-18 15:44:15 +0900

    Tweak codes for testing

--- clj/template/run_test_var.clj
+++ clj/template/run_test_var.clj
@@ -1,13 +1,14 @@
-(require 'clojure.test)
-(let [current-report (atom {})
-      report (fn [m]
-               (case (:type m)
-                 :pass (swap! current-report update (:type m) (fnil inc 0))
-                 :fail  (swap! current-report update (:type m) (fnil inc 0))
-                 :error (swap! current-report update (:type m) (fnil inc 0))
-                 :begin-test-var (swap! current-report update :test (fnil inc 0))
-                 nil))]
-  (binding [clojure.test/report report]
-    (clojure.test/test-var #'%s))
-  (merge {:test 0 :pass 0 :fail 0 :error 0}
-         @current-report))
+(do (require 'clojure.test)
+    (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0})
+          report (fn [m]
+                   (case (:type m)
+                     :pass (swap! summary update (:type m)  inc)
+                     :fail  (swap! summary update (:type m) inc)
+                     :error (swap! summary update (:type m) inc)
+                     :begin-test-var (swap! summary update :test inc)
+                     nil))]
+      (binding [clojure.test/report report]
+        (clojure.test/test-var #'%s))
+      {:results []
+       :summary @summary
+       :testing-ns []}))
