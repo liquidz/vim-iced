c78d4530a536ae689739e828da9b10ac78b331d9
liquidz <liquidz.uo@gmail.com>
2020-06-19 06:25:22 +0900

    Update to capture current testing var

--- clj/template/run_test_var.clj
+++ clj/template/run_test_var.clj
@@ -1,14 +1,24 @@
 (do (require 'clojure.test)
     (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0})
+          errors (atom [])
+          testing-var (atom nil)
           report (fn [m]
                    (case (:type m)
                      :pass (swap! summary update (:type m)  inc)
-                     :fail  (swap! summary update (:type m) inc)
-                     :error (swap! summary update (:type m) inc)
-                     :begin-test-var (swap! summary update :test inc)
+                     :fail  (do (swap! summary update (:type m) inc)
+                                (swap! errors conj (assoc m
+                                                          :ns (namespace @testing-var)
+                                                          :var (name @testing-var))))
+                     :error (do (swap! summary update (:type m) inc)
+                                (swap! errors conj (assoc m
+                                                          :ns (namespace @testing-var)
+                                                          :var (name @testing-var))))
+                     :begin-test-var (do (swap! summary update :test inc)
+                                         (reset! testing-var (symbol (:var m))))
+                     :end-test-var (reset! testing-var nil)
                      nil))]
       (binding [clojure.test/report report]
         (clojure.test/test-var #'%s))
-      {:results []
-       :summary @summary
-       :testing-ns []}))
+      {:summary @summary
+       :errors @errors}))
+
