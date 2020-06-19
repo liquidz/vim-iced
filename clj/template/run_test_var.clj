702c23e4c453708ee320e9c58db61b64416b5525
liquidz <liquidz.uo@gmail.com>
2020-06-20 07:53:39 +0900

    Update to be able to return `testing-ns`

--- clj/template/run_test_var.clj
+++ clj/template/run_test_var.clj
@@ -2,10 +2,11 @@
     (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0 :var 0})
           results (atom {})
           testing-var (atom nil)
+          testing-ns (atom nil)
           to-str (fn [x]
-                  (if (instance? Throwable x)
-                    (str (class x) ": " (.getMessage x) (ex-data x))
-                    (pr-str x)))
+                   (if (instance? Throwable x)
+                     (str (class x) ": " (.getMessage x) (ex-data x))
+                     (pr-str x)))
           report (fn [m]
                    (case (:type m)
                      :pass (swap! summary update (:type m)  inc)
@@ -18,13 +19,16 @@
                                       (swap! summary update (:type m) inc)
                                       (swap! results update-in [ns' var'] conj m))
 
-                     :begin-test-var (do (swap! summary update :test inc)
-                                         (swap! summary update :var inc)
-                                         (reset! testing-var (symbol (:var m))))
+                     :begin-test-var (let [var-sym (symbol (:var m))]
+                                       (swap! summary update :test inc)
+                                       (swap! summary update :var inc)
+                                       (reset! testing-var var-sym)
+                                       (reset! testing-ns (namespace var-sym)))
 
                      :end-test-var (reset! testing-var nil)
                      nil))]
       (binding [clojure.test/report report]
         (clojure.test/test-var #'%s))
-      {:summary @summary
-       :results @results}))
+      (cond-> {:summary @summary
+               :results @results}
+        @testing-ns (assoc :testing-ns @testing-ns))))
