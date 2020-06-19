a901ee08fbf06292dec9fcc6a716cbb9b3fa7dae
liquidz <liquidz.uo@gmail.com>
2020-06-19 09:57:20 +0900

    Update to return test results with similar structure as cider-nrepl

--- clj/template/run_test_var.clj
+++ clj/template/run_test_var.clj
@@ -1,24 +1,23 @@
 (do (require 'clojure.test)
-    (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0})
-          errors (atom [])
+    (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0 :var 0})
+          results (atom {})
           testing-var (atom nil)
           report (fn [m]
                    (case (:type m)
                      :pass (swap! summary update (:type m)  inc)
-                     :fail  (do (swap! summary update (:type m) inc)
-                                (swap! errors conj (assoc m
-                                                          :ns (namespace @testing-var)
-                                                          :var (name @testing-var))))
-                     :error (do (swap! summary update (:type m) inc)
-                                (swap! errors conj (assoc m
-                                                          :ns (namespace @testing-var)
-                                                          :var (name @testing-var))))
+
+                     (:fail :error) (let [ns' (namespace @testing-var)
+                                          var' (name @testing-var)]
+                                      (swap! summary update (:type m) inc)
+                                      (swap! results update-in [ns' var'] conj (assoc m :ns ns' :var var')))
+
                      :begin-test-var (do (swap! summary update :test inc)
+                                         (swap! summary update :var inc)
                                          (reset! testing-var (symbol (:var m))))
+
                      :end-test-var (reset! testing-var nil)
                      nil))]
       (binding [clojure.test/report report]
         (clojure.test/test-var #'%s))
       {:summary @summary
-       :errors @errors}))
-
+       :results @results}))
