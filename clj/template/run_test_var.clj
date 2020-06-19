1535f402600093479e0afd0f15d589caa384de59
liquidz <liquidz.uo@gmail.com>
2020-06-20 07:25:59 +0900

    Fix to convert Throwable to String

--- clj/template/run_test_var.clj
+++ clj/template/run_test_var.clj
@@ -2,14 +2,21 @@
     (let [summary (atom {:test 0 :pass 0 :fail 0 :error 0 :var 0})
           results (atom {})
           testing-var (atom nil)
+          to-str (fn [x]
+                  (if (instance? Throwable x)
+                    (str (class x) ": " (.getMessage x) (ex-data x))
+                    (pr-str x)))
           report (fn [m]
                    (case (:type m)
                      :pass (swap! summary update (:type m)  inc)
 
                      (:fail :error) (let [ns' (namespace @testing-var)
-                                          var' (name @testing-var)]
+                                          var' (name @testing-var)
+                                          m (-> (assoc m :ns ns' :var var')
+                                                (update :expected to-str)
+                                                (update :actual to-str))]
                                       (swap! summary update (:type m) inc)
-                                      (swap! results update-in [ns' var'] conj (assoc m :ns ns' :var var')))
+                                      (swap! results update-in [ns' var'] conj m))
 
                      :begin-test-var (do (swap! summary update :test inc)
                                          (swap! summary update :var inc)
