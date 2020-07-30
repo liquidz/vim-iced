d763599cd2a2433c3caa75bdb97c7dafa99dda9f
liquidz <liquidz.uo@gmail.com>
2020-07-30 13:45:33 +0900

    Add tests for read-string*

--- test/clj/script/lein_is_using_cljs_test.clj
+++ test/clj/script/lein_is_using_cljs_test.clj
@@ -3,11 +3,31 @@
    [clojure.test :as t]
    [lein-is-using-cljs :as sut]))
 
+(t/deftest read-string*-test
+  (t/testing "multiple expressions"
+    (t/is (= '[(hello) (world)]
+             (sut/read-string* "(hello) (world)"))))
+  (t/testing "tilde"
+    (t/is (= '[(hello world)]
+             (sut/read-string* "(hello ~world)"))))
+  (t/testing "regexp"
+    (t/is (= '[(hello "world")]
+             (sut/read-string* "(hello #\"world\")"))))
+  (t/testing "#="
+    (t/is (= '[(hello (slurp "world.txt"))]
+             (sut/read-string* "(hello #=(slurp \"world.txt\"))"))))
+  (t/testing "#()"
+    (t/is (= '[(hello (world))]
+             (sut/read-string* "(hello #(world))"))))
+  (t/testing "#{}"
+    (t/is (= '[(hello #{world})]
+             (sut/read-string* "(hello #{world})")))))
+
 (t/deftest using-cljs?-test
-  (t/is (true?  (sut/using-cljs?
-                 "(defproject foo \"0.1.0\"
-                        :dependencies [[foo \"xxx\"]
-                                       [org.clojure/clojurescript \"xxx\"]])")))
+  (t/is (true? (sut/using-cljs?
+                "(defproject foo \"0.1.0\"
+                   :dependencies [[foo \"xxx\"]
+                                  [org.clojure/clojurescript \"xxx\"]])")))
 
   (t/is (false? (sut/using-cljs?
                  "(defproject foo \"0.1.0\"
