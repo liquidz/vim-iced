243571395bef771e3b20bd533a2919e83dca34d7
liquidz <liquidz.uo@gmail.com>
2024-02-28 22:21:14 +0900

    fix: Update cljstyle installer

--- installer/cljstyle.sh
+++ installer/cljstyle.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-version='0.15.0'
-file='cljstyle.tar.gz'
+version='0.16.626'
+file='cljstyle.zip'
 if [ "$(uname)" == 'Darwin' ]; then
     os='macos'
 else
     os='linux'
 fi
-url="https://github.com/greglook/cljstyle/releases/download/${version}/cljstyle_${version}_${os}.tar.gz"
+arch="$(uname -m)"
+url="https://github.com/greglook/cljstyle/releases/download/${version}/cljstyle_${version}_${os}_${arch}.zip"
 
-curl -L -o ${file} ${url} && tar xvf ${file} && rm -f ${file}
+curl -L -o ${file} ${url} && unzip ${file} && rm -f ${file}
